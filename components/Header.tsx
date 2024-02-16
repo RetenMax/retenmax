7d50523197968ee5a0ee36b484264b9e60c47202
Kaio Espindola <oemaildokaio@gmail.com>
2024-02-16 08:57:22 -0400

    [FD] Created Header component

---
+++ components/Header.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import { Button } from "./ui/button"
+
+const Header = () => {
+    return(
+        <header className="sticky top-0 backdrop-blur-md bg-black/10 w-full pt-6 pb-6">
+            <div className="container mx-auto flex justify-between">
+                <Image
+                    src={"/images/logo.svg"}
+                    alt="Retenmax Logo"
+                    width={160}
+                    height={32}
+                />
+                <div className="flex gap-4">
+                    <Button variant={'ghost'}>Podcast</Button>
+                    <Button variant={'ghost'}>Serviços</Button>
+                    <Button variant={'ghost'}>Clientes</Button>
+                    <a href="#">
+                        <Button>Entre em contato</Button>
+                    </a>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
---
+++ components/Hero.tsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+    return(
+        <div className=" w-full h-96">
+
+        </div>
+    )
+}
+
+export default Hero
