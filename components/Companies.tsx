a847abec698c98a086c3bf2948306d2116b5aef9
Kaio Espindola <kaio.espindola@osf.digital>
2024-03-02 10:34:20 -0400

    [FD] Updated companies logo

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -9,7 +9,7 @@ const Companies = () => {
         <section id="empresas"
             className="w-full h-[600px] bg-[url('/images/bg-companies.webp')] bg-center bg-cover">
             <div className="container grid h-full lg:grid-cols-3">
-                <div className="flex justify-center items-center col-span-2">
+                <div className="flex flex-col justify-center items-center col-span-2">
                     <motion.img
                         initial={{ opacity: 0, transform: "translateY(40px)" }}
                         whileInView={{ opacity: 1, transform: "translateY(0)" }}
@@ -17,8 +17,38 @@ const Companies = () => {
                         className="mix-blend-lighten"
                         width={320}
                         height={400}
-                        alt="Retenmax Empresas"
-                        src="/images/companies.webp"
+                        alt="RetenMax"
+                        src="/images/logo.svg"
+                    />
+                    <motion.img
+                        initial={{ opacity: 0, transform: "translateY(40px)" }}
+                        whileInView={{ opacity: 1, transform: "translateY(0)" }}
+                        transition={{ ease: 'easeInOut', duration: 1.5 }}
+                        className="mix-blend-lighten"
+                        width={320}
+                        height={400}
+                        alt="Finmax - RetenMax"
+                        src="/images/finmax.webp"
+                    />
+                    <motion.img
+                        initial={{ opacity: 0, transform: "translateY(40px)" }}
+                        whileInView={{ opacity: 1, transform: "translateY(0)" }}
+                        transition={{ ease: 'easeInOut', duration: 1.5 }}
+                        className="mix-blend-lighten"
+                        width={320}
+                        height={400}
+                        alt="RMax - RetenMax"
+                        src="/images/rmax.webp"
+                    />
+                    <motion.img
+                        initial={{ opacity: 0, transform: "translateY(40px)" }}
+                        whileInView={{ opacity: 1, transform: "translateY(0)" }}
+                        transition={{ ease: 'easeInOut', duration: 1.5 }}
+                        className="mix-blend-lighten"
+                        width={200}
+                        height="auto"
+                        alt="RMaster Empresas"
+                        src="/images/rmaster.webp"
                     />
                 </div>
                 <motion.div
