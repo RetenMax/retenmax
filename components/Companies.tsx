ca4c918e7dc24714d3e4514ff2fbd14c3ba8c423
Kaio Espindola <kaio.espindola@osf.digital>
2024-03-02 15:17:56 -0400

    Fix minor

--- components/Companies.tsx
+++ components/Companies.tsx
@@ -7,9 +7,9 @@ import { motion } from "framer-motion"
 const Companies = () => {
     return(
         <section id="empresas"
-            className="w-full h-[600px] bg-[url('/images/bg-companies.webp')] bg-center bg-cover">
+            className="w-full lg:h-[600px] bg-[url('/images/bg-companies.webp')] bg-center bg-cover">
             <div className="container grid h-full lg:grid-cols-3">
-                <div className="flex flex-col justify-center items-center col-span-2">
+                <div className="flex flex-col justify-center items-center col-span-2 p-12">
                     <motion.img
                         initial={{ opacity: 0, transform: "translateY(40px)" }}
                         whileInView={{ opacity: 1, transform: "translateY(0)" }}
@@ -55,7 +55,7 @@ const Companies = () => {
                     initial={{ opacity: 0, z: 1, transform: "translateY(40px)" }}
                     whileInView={{ opacity: 1, z: 1, transform: "translateY(0)" }}
                     transition={{ ease: 'easeInOut', duration: 1.5 }}
-                    className="w-full flex flex-col justify-center items-center min-h-full">
+                    className="w-full flex flex-col justify-center items-center min-h-full  pb-12 lg:p-2">
                     <h1 className="leading-[46px] text-5xl font-extrabold antialiased text-orange-100 lg:text-7xl ">
                         Retenção em todas as verticais
                     </h1>
