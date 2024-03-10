a117e258db45b5036682bc334e930e74b49e14f2
Kaio Espindola <kaio.espindola@osf.digital>
2024-03-10 12:33:21 -0400

    Integration - Menu Backend

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,20 +1,15 @@
-"use client"
-
 import Image from "next/image"
 import { Button } from "./ui/button"
 import Link from "next/link"
+import { getMainMenu } from "@/lib/api"
 
-import { motion } from "framer-motion"
+const Header = async () => {
 
-const Header = () => {
+    const menu = await getMainMenu()
+
     return(
         <header className="fixed top-0 backdrop-blur-md bg-black/10 w-full pt-6 pb-6 z-10">
-            <motion.div
-                initial={{ opacity: 0 }}
-                whileInView={{ opacity: 1 }}
-                viewport={{ once: true }}
-                transition={{ ease: 'easeInOut', duration: 1 }}
-                className="container mx-auto flex justify-between">
+            <div className="container mx-auto flex justify-between">
                 <Link href="/">
                     <Image
                         src={"/images/logo.svg"}
@@ -24,23 +19,15 @@ const Header = () => {
                     />
                 </Link>
                 <div className="hidden lg:flex gap-4 visible">
-                    <Link href="/blog">
-                        <Button variant={'ghost'}>Blog</Button>
-                    </Link>
-                    <Link href="/#podcast">
-                        <Button variant={'ghost'}>Podcast</Button>
-                    </Link>
-                    <Link href="/#empresas">
-                        <Button variant={'ghost'}>Serviços</Button>
-                    </Link>
-                    <Link href="/#clientes">
-                        <Button variant={'ghost'}>Clientes</Button>
-                    </Link>
-                    <Link href="/#contato">
-                        <Button>Entre em contato</Button>
-                    </Link>
+                    {menu.map(menu => {
+                        return(
+                            <Link key={menu.id} href={menu.Link}>
+                                <Button variant={'ghost'}>{menu.Titulo}</Button>
+                            </Link>
+                        )
+                    })}
                 </div>
-            </motion.div>
+            </div>
         </header>
     )
 }
