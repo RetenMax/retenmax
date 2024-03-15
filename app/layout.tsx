54f888f3f63ef2270a47ba5cbccc05acf5264939
Kaio Espindola <kaio.espindola@osf.digital>
2024-03-14 20:27:35 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,21 @@ export const metadata: Metadata = {
     keywords: "consultoria empresarial, software empresarial, campo grande ms, retenmax, retenção de clientes, retenção de clientes campo grande ms, retenção de clientes ms, retenção de clientes mato grosso do sul, retenção de clientes brasil, retenção de clientes sul, retenção de clientes centro-oeste, retenção de clientes norte, retenção de clientes nordeste, retenção de clientes sudeste, retenção de clientes suldeste, retenção de clientes mato grosso do sul, retenção de clientes mato grosso, retenção de clientes goiás, retenção de clientes são paulo, retenção de clientes rio de janeiro, retenção de clientes minas gerais, retenção de clientes espírito santo, retenção de clientes paraná, retenção de clientes santa catarina, retenção de clientes rio grande do sul, retenção de clientes mato grosso do sul, retenção de clientes mato grosso do sul, retenção de clientes mato grosso do sul, retenção de clientes mato grosso do sul, retenção de clientes mato grosso do sul ",
     authors: { name: "Arthur Maximiliano" },
     viewport: "width=device-width, initial-scale=1.0",
-    robots: "follow, index"
+    robots: "follow, index",
+    openGraph: {
+        type: "website",
+            locale: "pt_BR",
+            url: "https://retenmax.com.br",
+            siteName: "Retenmax - Retenção máxima de clientes",
+            images: [
+                {
+                    url: "https://retenmax.com.br/favicon.ico",
+                    width: 128,
+                    height: 128,
+                    alt: "Retenmax - Retenção máxima de clientes",
+                },
+            ],
+    }
 };
 
 export default function RootLayout({ children }: Readonly<{children: React.ReactNode}>) {
