37c15a769b654ef2b3eb9ceb8778701bd6d3d0f2
PedroPaulo <131112587+EspindolaPedro@users.noreply.github.com>
2025-03-13 11:50:11 -0400

    Número alterado

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -19,7 +19,7 @@ const Contact = () => {
                         Vamos <br></br>Conversar?
                     </h1>
                     <p className="text-orange-100">Nós ajudamos a sua empresa a aprimorar a retenção de clientes e a de colaboradores.</p>
-                    <Link href="https://api.whatsapp.com/send?phone=5567999502501" target="_blank">
+                    <Link href="https://api.whatsapp.com/send?phone=556799438628&text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20da%20RetenMax." target="_blank">
                         <Button className="flex gap-2 hover:scale-95 transition-transform duration-300" variant="destructive">
                         Descobrir o Potencial da Minha Empresa <ArrowRightCircle className="h-4 w-4" />
                         </Button>
